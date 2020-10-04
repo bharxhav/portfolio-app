b4acdc6653eb68ed24971f33156dee6ef73041f0
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2020-10-04 15:46:10 +0530

    Add design text

--- lib/home.dart
+++ lib/home.dart
@@ -10,12 +10,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    // double _width = MediaQuery.of(context).size.width;
+    // bool darkTheme = true;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Bhargav Kantheti'),
           leading: Icon(Icons.account_circle),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/bharxhav.png',
+                fit: BoxFit.fitHeight,
+                width: 150,
+              ),
+              Text(
+                'Bhargav Kantheti',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  // color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: 'SansitaSwashed',
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 1
+                    ..color = Colors.orange[700],
+                ),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
         ),
       ),
       theme: ThemeData.dark(),
