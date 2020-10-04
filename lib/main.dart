a2ac57842672e19efca02e9346d28d69f3dc45f9
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2020-10-04 14:32:41 +0530

    Add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() => runApp(MaterialApp(home: HomePage()));
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
